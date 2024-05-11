43e2f0c4965f41548e238d46c33c16d3c0388413
yk884 <takosaex@gmail.com>
2024-05-11 14:37:15 +0900

    create login form

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Login.scss";
 
 const Login = () => {
-  return <div>Login Component</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <div className="loginWrapper">
+      <div className="loginForm">
+        <div className="loginFormTitle">
+          <h2>ログイン</h2>
+        </div>
+        <div className="loginFormInputContainer">
+          <p className="inputTitle">メールアドレス</p>
+          <input
+            type="email"
+            placeholder="sample@example.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="loginFormInputContainer">
+          <p className="inputTitle">パスワード</p>
+          <input
+            type="password"
+            placeholder=""
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button className="button loginButton">ログイン</button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
